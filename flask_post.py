aab46af3cec21ecc687f9012d0cfcd9b56f875f3
ananthaa-advisory <aravindaravind53@gmail.com>
2020-04-22 13:44:15 +0530

    Flask_post.py - Web Application to post Data
    This is a web application which takes input in the form and display the output in the HTML format..
    Note -  This can be further drilled down to make the inputs pass to ICINGA API and process downtime accordingly, infact that is the basic agenda and this is just a BASIC form to accept FORM input and display output in HTML format.

--- flask_post.py
+++ flask_post.py
@@ -1,9 +1,8 @@
+import sys
 from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__, template_folder="/root/templates")
 
-
 @app.route('/search', methods=["GET","POST"])
-
 
 # If method is POST it will submit the values to POST call else it will display the POST.HTML form only. 
 #THe POST HTML form will ask for user details like First name and Last name
@@ -12,17 +11,21 @@ app = Flask(__name__, template_folder="/root/templates")
 
 # Part 2 - Here again one more HTML call post2.html is invoked to display the results. Refer the image files respectively on the forms
 
+
 def entry():
     if request.method == "POST":
         name = request.form["name"]
         lname = request.form["lname"]
-        return search(name,lname)
-      
+        comment = request.form["comment"]
+        return search(name,lname,comment)
+
     else:
         return render_template("post.html")
 
-def search(name1,lname1):
-    return render_template("post2.html", the_name1 = name1, the_lname1 = lname1)
+def search(name1,lname1,comment):
+# THe below variable just for print to pass the HOSTNAME input to ICINGA API and process downwtime accordingly
+    print(type(lname1))
+    return render_template("post2.html", the_name1 = name1, the_lname1 = lname1, the_comment = comment)
 
 
 
